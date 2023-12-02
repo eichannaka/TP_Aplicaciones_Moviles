f0c92a51d388e05e9b67f43275196659ebe4cde2
eichannaka <alannkgw@hotmail.com>
2023-12-02 02:32:02 -0300

    Pagina login,welcome y main

--- app/src/main/java/com/example/tp_aplicacion_movil/WelcomeActivity.java
+++ app/src/main/java/com/example/tp_aplicacion_movil/WelcomeActivity.java
@@ -2,7 +2,11 @@ package com.example.tp_aplicacion_movil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button buttonWelcome = findViewById(R.id.button_welcome);
+        buttonWelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aquí se ejecutará el código cuando se presione el botón
+                // Por ejemplo, iniciar la actividad de inicio de sesión
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

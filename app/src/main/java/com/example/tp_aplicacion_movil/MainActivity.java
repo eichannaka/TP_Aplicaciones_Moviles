0769f9bd581bf9d9c0899e89f236384d2af725e9
eichannaka <alannkgw@hotmail.com>
2023-12-05 01:21:17 -0300

    Funcionalidad de imageButton, Uso de ScrollView para los ejercicio de pecho, hay que agregar los otros musculos y sus rutinas

--- app/src/main/java/com/example/tp_aplicacion_movil/MainActivity.java
+++ app/src/main/java/com/example/tp_aplicacion_movil/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tp_aplicacion_movil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Encuentra tus ImageButtons por sus IDs
+        ImageButton btnPecho = findViewById(R.id.idPecho);
+
+        // Agrega un listener al ImageButton
+        btnPecho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Configura el intent para iniciar la nueva actividad
+                Intent intent = new Intent(MainActivity.this, PechoEjercicio.class);
+                startActivity(intent);
+            }
+        });
     }
+    // Otro código de la actividad principal
 }
--- app/src/main/java/com/example/tp_aplicacion_movil/PechoEjercicio.java
+++ app/src/main/java/com/example/tp_aplicacion_movil/PechoEjercicio.java
@@ -1,4 +1,14 @@
 package com.example.tp_aplicacion_movil;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class PechoEjercicio {
+import android.os.Bundle;
+
+public class PechoEjercicio extends  AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejercicio_pecho);
+
+        // Puedes realizar más configuraciones o acciones específicas para esta actividad aquí
+    }
 }

fef635377aca57ab9f604e4ffed9e7054f98826b
eichannaka <alannkgw@hotmail.com>
2023-12-07 19:32:35 -0300

    Correcciones

--- app/src/main/java/com/example/tp_aplicacion_movil/MainActivity.java
+++ app/src/main/java/com/example/tp_aplicacion_movil/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.tp_aplicacion_movil;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ImageButtons de los musculos por sus IDs
+        // Resto de tu código actual
         ImageButton btnPecho = findViewById(R.id.idPecho);
         ImageButton btnPierna = findViewById(R.id.idPierna);
         ImageButton btnEspalda = findViewById(R.id.idEspalda);
@@ -22,12 +26,12 @@ public class MainActivity extends AppCompatActivity {
         ImageButton btnAbdominales = findViewById(R.id.idAbdominales);
         ImageButton btnBiceps = findViewById(R.id.idBiceps);
 
+        // Agregar listeners a los ImageButtons existentes
 
-        // Agrega un listener al ImageButton Pecho
         btnPecho.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Configura el intent para iniciar la nueva actividad
+                // Configurar intent para iniciar la nueva actividad
                 Intent intent = new Intent(MainActivity.this, PechoEjercicio.class);
                 startActivity(intent);
             }
@@ -77,17 +81,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        Button btnAgregarRegistro = findViewById(R.id.btnAgregarRegistro);
+        Button btnRegistroActividades = findViewById(R.id.btnRegistroActividades);
 
-    }
+        btnAgregarRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Configurar intent para iniciar la actividad AgregarRegistro
+                Intent intent = new Intent(MainActivity.this, RegistroAgregar.class);
+                startActivity(intent);
+            }
+        });
 
-    public void goToAgregarRegistroLayout(View view) {
-        Intent intent = new Intent(this, RegistroFuncionalidad.class);
-        startActivity(intent);
-    }
-
-    public void goToRegistroActividadesLayout(View view) {
-        Intent intent = new Intent(this, RegistroFuncionalidad.class);
-        startActivity(intent);
+        btnRegistroActividades.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Configurar intent para iniciar la actividad RegistroActividades
+                Intent intent = new Intent(MainActivity.this, RegistroActividad.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/tp_aplicacion_movil/RegistroFuncionalidad.java
+++ app/src/main/java/com/example/tp_aplicacion_movil/RegistroFuncionalidad.java
@@ -1,5 +1,4 @@
 package com.example.tp_aplicacion_movil;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,23 +6,24 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
 public class RegistroFuncionalidad extends AppCompatActivity {
 
     private LinearLayout layoutRegistros;
     private EditText editTextTitulo;
     private EditText editTextDescripcion;
+    private ArrayList<String> registros; // ArrayList para almacenar los registros
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.agregar_registro);
-        setContentView(R.layout.registro_actividades);
-
+        setContentView(R.layout.registro_actividades); // Cambio a la actividad 'registro_actividades'
         editTextTitulo = findViewById(R.id.editTextTitulo);
         editTextDescripcion = findViewById(R.id.editTextDescripcion);
         Button btnAgregar = findViewById(R.id.btnAgregar);
         layoutRegistros = findViewById(R.id.layoutRegistros);
+        registros = new ArrayList<>(); // Inicializar el ArrayList
 
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,11 +37,25 @@ public class RegistroFuncionalidad extends AppCompatActivity {
         String titulo = editTextTitulo.getText().toString();
         String descripcion = editTextDescripcion.getText().toString();
 
-        // Crear una nueva vista para mostrar el registro
-        TextView nuevoRegistro = new TextView(this);
-        nuevoRegistro.setText("Título: " + titulo + "\nDescripción: " + descripcion);
+        // Crear un nuevo registro en formato de cadena
+        String nuevoRegistro = "Título: " + titulo + "\nDescripción: " + descripcion;
 
-        // Agregar la nueva vista al layout de registros
-        layoutRegistros.addView(nuevoRegistro);
+        // Agregar el registro al ArrayList
+        registros.add(nuevoRegistro);
+
+        // Actualizar la vista con todos los registros
+        actualizarRegistros();
+    }
+
+    private void actualizarRegistros() {
+        // Limpiar la vista antes de mostrar los registros actualizados
+        layoutRegistros.removeAllViews();
+
+        // Recorrer el ArrayList y agregar cada registro como un TextView al layout
+        for (String registro : registros) {
+            TextView textViewRegistro = new TextView(this);
+            textViewRegistro.setText(registro);
+            layoutRegistros.addView(textViewRegistro);
+        }
     }
 }
